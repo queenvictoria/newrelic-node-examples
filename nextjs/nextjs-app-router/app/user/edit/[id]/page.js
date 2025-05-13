5ba63e82d01d4d814276148418867ff0c61f03d2
Snow <snow@sbs.com.au>
2025-05-13 15:01:28 +1000

    Non-functional: Client side setCustomAttribute.

--- nextjs/nextjs-app-router/app/user/edit/[id]/page.js
+++ nextjs/nextjs-app-router/app/user/edit/[id]/page.js
@@ -25,13 +25,22 @@ export default function Page({ params }) {
     }
     , [id])
 
+  // use client doesn't prevent server-side rendering and therefore `window` is
+  // not always available. So wrap in a useEffect to ensure this code only runs
+  // on the client.
+  useEffect(() => {
+    console.log('Set custom attribute userId:', id)
+    window.newrelic.setCustomAttribute("userId", id)
+  }
+  , [id])
+
   if (isLoading === true) return <p>Loading...</p>
   if (!user) return notFound()
 
   // Instrumenting client pages:
-  // Using New Relic API methods in client pages will require the New Relic Browser Agent, 
+  // Using New Relic API methods in client pages will require the New Relic Browser Agent,
   // which is available on the global `window` object:
-  // window.newrelic.setCustomAttribute("customAttribute", 'custom attribute value')
+  window.newrelic.setCustomAttribute("customAttribute", 'custom attribute value')
 
   async function onSubmit(event) {
     event.preventDefault()
